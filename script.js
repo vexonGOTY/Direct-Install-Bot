b4b838bc610400c8b0e11875a5d2ac6d88b26719
Vexon <luzi@login.ch>
2026-03-08 16:49:15 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Thin loader to keep the main implementation in app.js
-// while exposing a script.js entrypoint for the static site.
-(() => {
-  const script = document.createElement("script");
-  script.src = "./app.js";
-  script.async = false;
-  document.head.appendChild(script);
-})();
-
